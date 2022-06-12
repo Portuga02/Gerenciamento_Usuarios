214432f1dc069cda168ee087485b221f7c12ff3c
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-06-12 15:29:33 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-alert("Olá mundo")
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll(
+  "#form-user-create [name=gender]:checked"
+);
+
+var birth = document.querySelector("#exampleInputBirth");
+
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelectorAll("#exampleInputAdmin");
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function (field, index) {
+    
+    console.log(field);
+    console.log( index);
+});
